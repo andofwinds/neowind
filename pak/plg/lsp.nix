0c528e19dbbd1e05e64bd526704b47e768ad9334
andofwinds <andrey@elib.ru>
2024-07-06 14:57:46 +0300

    Add C / Nix LSP

--- pak/plg/lsp.nix
+++ pak/plg/lsp.nix
@@ -1,17 +1,20 @@
 { ... }:
 {
-  plugins.cmp-nvim-lsp.enable = true;
+  plugins = {
+		cmp-nvim-lsp.enable = true;
+		lsp = {
+			enable = true;
 
-  plugins.lsp = {
-    enable = true;
-
-    servers = {
-      rust-analyzer = {
-        enable = true;
-	installRustc = true;
-	installCargo = true;
-      };
-      tsserver.enable = true;
-    };
-  };
+			servers = {
+				rust-analyzer = {
+					enable = true;
+					installRustc = true;
+					installCargo = true;
+				};
+				tsserver.enable = true;
+				nil-ls.enable = true;
+				clangd.enable = true;
+			};
+		};
+	};
 }
