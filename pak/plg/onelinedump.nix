d0835f7cb293136c67fc6136a8e2159d095e3832
andofwinds <=>
2024-07-20 20:11:11 +0300

    nvim-colorizer

--- pak/plg/onelinedump.nix
+++ pak/plg/onelinedump.nix
@@ -3,6 +3,7 @@
   plugins = {
     nvim-autopairs.enable = true;
     nvim-tree.enable = true;
+		nvim-colorizer.enable = true;
     toggleterm.enable = true;
     lualine.enable = true;
 		markdown-preview.enable = true;
