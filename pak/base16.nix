7d4a00f1f2791d1967d0aeee6bf4b47e5bed47dd
andofwinds <=>
2025-02-19 15:26:19 +0300

    Add more doc in pak/base16.nix & Swap Comment and Plain Text colors

--- pak/base16.nix
+++ pak/base16.nix
@@ -1,18 +1,18 @@
 {
-	base00 = "#2F3661";	#
-	base01 = "#4E668F";	#
+	base00 = "#2F3661";	# Background.
+	base01 = "#4E668F";	# Caret row color.
 	base02 = "#2E325C";	# Branch and File percentage BG, Visual mode selection.
-	base03 = "#BFC8D7";	# Comments.
-	base04 = "#7D98C8";
-	base05 = "#7E89AC"; # Branch and File percentage FG, Parens, Semicolons, File name.
+	base03 = "#7E89AC";	# Comments.
+	base04 = "#7D98C8"; # Line number, File / Language name, CR/NL, Encoding.
+	base05 = "#BFC8D7"; # Branch and File percentage FG, Parens, Semicolons, File name, Plain text.
 	base06 = "#E2E2EC";
 	base07 = "#A8AED2";
 	base08 = "#DAB4C8";	# Reserved symbols, variables.
 	base09 = "#608FD9";	# Numbers, Enum variants.
 	base0A = "#9C8AB4";
-	base0B = "#9DBAD7"; # INSERT Mode, Include's file name.
+	base0B = "#9DBAD7"; # INSERT Mode, Include's file name, Caret position.
 	base0C = "#A1B9DD";	#
 	base0D = "#7283B0";	# NORMAL Mode, Line, Function name.
 	base0E = "#9C8AB4"; # Reserved symbols, types.
-	base0F = "#6F82AB";	#
+	base0F = "#6F82AB";	# Colons.
 }
